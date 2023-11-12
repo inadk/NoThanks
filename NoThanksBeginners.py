2f577936817bc99dc1d4c57fd11f1ba0d693430a
inadk <inadiatlok@gmail.com>
2023-11-13 00:21:13 +0100

    functions and penaltyless changes

--- NoThanksBeginners.py
+++ NoThanksBeginners.py
@@ -69,7 +69,7 @@ class CoinsDependent(Player):
         return True
 
 
-
+# -----------------------------------------------------------------------------------
 # create a new class inheriting features from Player - defined in NoThanksPlayer.py
 class MyAI(Player):
     # create init method 
@@ -96,45 +96,72 @@ class MyAI(Player):
 # if no coins for player, take is not called - automatically takes cards and coins
 
     def take( self, card, state ):
+
         if self.coins <= 0:
             return True
 
-        # if card is not my collection and not in any of my neighbours collection, get coins on it before taking (whenever possible)
-        if myNeighbour(self, card):
-            if someonesNeighbour(self, card, state):
-                return True
-            if coinlessPlayer(self, state):
-                return True
-            if card.number / (card.coins + 4) < 2.5:
-                return True
-            return False           
+        takeNeighbour(self, card, state)
 
-        # only take penaltiless card at round 24-n (n=1,2,3), except when there are less than (n+1) players with less coins than me
-        if state.round == 23:
-            if coinRank(self, state) <= 3:
-                return False
-        if state.round == 22:
-            if coinRank(self, state) <= 2:
-                return False        
+        roundSpecificAction(self, state, card)
+
+        whenFewCoins(self, card, state, 4, 16, 4)
+
+        goodCoinPenaltyRatio(card, 2)
+        
+        # if xPenalty(self, card, state) >= 0:
+        #     return True
+    
+        return False
+
+    
+# functions
+
+# very important ones
+# 1.
+def roundSpecificAction(self, state, card):
+    if self.penaltyWhenTake( card ) >= self.penalty():
         if state.round == 21:
             if coinRank(self, state) <= 1:
                 return False
+        if state.round == 22:
+            if coinRank(self, state) <= 2:
+                return False
+        if state.round == 23:
+            if coinRank(self, state) <= 3:
+                return False
+        if state.round == 24:
+            return False
+        
+# 2.
+def dataCollector(card, state):
+    if state.round == 1:
+        cardsInPlay = [i for i in range(3, 36)]
+    cardsInPlay.remove(card.number)
 
-        # coins worth more than penalty (3x) when I have only a few coins
-        if self.coins <= 4 and state.round <= 16:
-            if card.coins >= ( self.penaltyWhenTake( card ) / ( pow( 2, (5 - self.coins ) ) ) ):
-                return True    
-
-        # take only if it results in collecting penalty and coins in at least 4/3 ratio
-        if card.coins >= ( self.penaltyWhenTake( card ) / 1.2 ):
+# 3.
+# if card is not my collection and not in any of my neighbours collection, get coins on it before taking (whenever possible)
+def takeNeighbour(self, card, state):
+    if myNeighbour(self, card):
+        if someonesNeighbour(self, card, state):
+            return True
+        if coinlessPlayer(self, state):
+            return True
+        if card.number / (card.coins + 4) < 2.5:
             return True
         return False
-    
+
+# 4.
+def whenFewCoins(self, card, state, ncoins, untilState, r):
+    if self.coins <= ncoins and state.round <= untilState:
+        if card.coins >= ( card.number / r ):
+            return True
+# 5.        
+def goodCoinPenaltyRatio(card, r):
+    if card.coins >= ( card.number / r ):
+        return True
+    return False
 
 
-    
-
-    
 def someonesNeighbour( self, card, state):
     for i in state.players:
         if i.name != self.name:
@@ -161,3 +188,43 @@ def coinRank(self, state):
         if i.coins >= self.coins:
             cr += 1
     return cr
+
+def numberOfNthNeighboursAtThem(self, card, state, n):
+    counter = 2
+    for i in state.players:
+        for j in i.collection:
+            if (card.number + n  == j.number) or (card.number -n  == j.number):
+                counter -= 1
+    return counter
+
+def numberOfNthNeighboursAtMe(self, card, n):
+    counter = 0
+    for i in self.collection:
+        if (card.number + 1  == i.number) or (card.number -n  == i.number):
+            counter += 1
+    return counter
+
+def uncertainty(state):
+    counter = 0
+    for i in state.players:
+        counter += pow(i.coins, 2)
+    return 5*11*11 / counter 
+
+def xPenalty(self, card, state):
+    n1them = numberOfNthNeighboursAtThem(self, card, state, 1)
+    n1me = numberOfNthNeighboursAtMe(self, card, 1)
+    n2them = numberOfNthNeighboursAtThem(self, card, state, 2)
+    n2me = numberOfNthNeighboursAtMe(self, card, 2)
+    deckSize = 24 - state.round
+    
+    xPR = card.number * (deckSize / (deckSize + 9) ) * (2 - n1them) * n2me * (1 - n2them / 4) * uncertainty(state) - card.number + card.coins
+    
+    if xPR >= 0:
+        print("Round: " + str(state.round))
+        print("xPR: " + str(xPR) + ", uncertainty: " + str(uncertainty(state)))
+        print("Card: " + str(card.number) + ', Coins: ' + str(card.coins))
+        print("n1them" + str(n1them) + ", n1me: " + str(n1me) + "n2them" + str(n2them) + ", n2me: " + str(n2me))
+        print("My collection: " + str(self.collection))
+        print("------")
+
+    return xPR
